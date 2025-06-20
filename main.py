720363ee748f6928b863ee47b12f189ed5d1856e
Tanzil Chowdhury <tanzilchowdhury2014@gmail.com>
2025-06-20 19:52:01 +0100

    Working version

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 def message_to_num(m):
     """Converts each letter in a string to its ascii value to create a number
      NOTE: ALL LETERS WILL BE CAPITALISED!"""
@@ -7,19 +12,31 @@ def message_to_num(m):
         num_m += str(ord(char))
     return int(num_m)
 
-def create_padlock(p1, p2, e=3):
+def num_to_message(m):
+    num_text = str(m)
+    text = ""
+    for i in range(0, len(num_text), 2):
+        text += chr(int(num_text[i]+num_text[i+1]))
+    return text
+
+def create_padlock(p1, p2):
     """Returns 2 values n (our modulo), e (our exponent) for encrypting message"""
     n = p1 * p2
-    return n, e
-
-def create_key(p1, p2, e):
     t = (p1-1)*(p2-1)
-    k=0
-    d = 0.1
-    while d != int(d):
-       k += 1
-       d = ((k*t)+1)/e
-    return int(d)
+    e = 0
+    for e in range(2, t):
+        if gcd(e, t) == 1:
+            break
+    return n, e, t
+
+def create_key(t, e):
+    k = 0  # Defining k
+    d = 1
+    # Loop to ensure that private key is an integer
+    while d%e != 0:
+        k += 1
+        d = (1+k*t)
+    return d//e
 
 def decrypt_message_to_num(c, d, n):
     return pow(c,d,n)
@@ -27,55 +44,51 @@ def decrypt_message_to_num(c, d, n):
 def encrypt_message(num_m,e,n):
     return pow(num_m, e, n)
 
-def validate_e(e, t):
-    # Assumes prime input
-    if t/e == int(t/e):
-        return False
-    if t <= e:
-        return False
-    return True
-
 def main():
+    print("Bob Side: ")
+    m = input("Enter Message: ")
+    input("Press enter for Alice to generate the keys. ")
+
     print("Alice Side: ")
-    p1 = int(input("Enter prime number 1: "))
-    p2 = int(input("Enter prime number 2: "))
+    p1 = 427339429644236995442992359307
+    p2 = 469334883452600878339466720693
 
-    n, e = create_padlock(p1, p2)
-    d = create_key(p1, p2, e)
+    n, e, t = create_padlock(p1, p2)
+    print("Generated n and e")
+    d = create_key(t, e)
+    print("Generated d")
 
-    input("Press enter to send off n and e to Bob. ")
-    print("n and e sent off but Alice now also has them!")
+    input("Press enter to send off n and e to Bob but not d.")
+    print("n and e sent off but Eve now also has them!")
 
     print("Bob Side")
-    m = input("Enter Message: ")
+    print("Converting our sentence to numbers.")
     num_m = message_to_num(m)
     print(num_m)
     c = encrypt_message(num_m, e, n)
+    print("Encrypted the message using e and n")
 
     input("Press enter to send off c to Alice. ")
-    print("c sent off but Alice now also has it!")
+    print("c sent off but Eve now also has it!")
 
     print("Alice silde")
     decrypted_m = decrypt_message_to_num(c, d, n)
     print(decrypted_m)
+    print(num_to_message(decrypted_m))
 
 def test():
-    p11 = 93333257
-    p21 = 84784369
-    e = 65537
-    p1 = 731742727
-    p2 = 757461871
-    n, e = create_padlock(p1, p2, e)
-    if not validate_e(e, ((p1-1)*(p2-1))):
-        return -1
+    p1 = 427339429644236995442992359307
+    p2 = 469334883452600878339466720693
+    n, e, t = create_padlock(p1, p2)
     print(f"modulo {n}, exponent {e}")
-    d = create_key(p1, p2, e)
+    d = create_key(t, e)
     print(f"private key {d}")
-    num_m = message_to_num("zzzzzzz")
+    num_m = message_to_num("LETS FUCKING JOEEEEEEE!!!")
     print(num_m)
     c = encrypt_message(num_m, e, n)
     print(f"cypher text {c}")
     decrypted_m = decrypt_message_to_num(c, d, n)
     print(decrypted_m)
+    print(num_to_message(decrypted_m))
 
-print(test())
+main()
